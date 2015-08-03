e8c22310cb8214e6091bdc6feb0775c8c37a4ba3
Mario Skouat <forum@skouat.org>
2015-08-03 13:40:16 +0200

    Add lang key for "donate" button

--- language/fr/donate.php
+++ language/fr/donate.php
@@ -36,6 +36,24 @@ if (empty($lang) || !is_array($lang))
 //
 
 $lang = array_merge($lang, array(
+	// Header
+	'PPDE_HEADER_LINK_TITLE'      => 'Faire un don',
+
+	// Index page
+	'PPDE_INDEX_STATISTICS_TITLE' => 'Statistiques des dons',
+
+	// Image alternative text
+	'IMG_LOADER'                  => 'chargement',
+
+	// Pages
+	'PPDE_DONATION_BUTTON_TITLE'  => 'Faire un don',
+	'PPDE_DONATION_TITLE'         => 'Faire un don',
+	'PPDE_DONATION_TITLE_HEAD'    => 'Faire un don pour',
+	'PPDE_CANCEL_TITLE'           => 'Dons annulés',
+	'PPDE_SUCCESS_TITLE'          => 'Dons validés',
+	'PPDE_CONTACT_PAYPAL'         => 'Connexion à PayPal - Veuillez patienter…',
+	'PPDE_SANDBOX_TITLE'          => 'Tester PayPal Donation avec PayPal Sandbox',
+
 	// Statistics
 	// Note for translators----->    %1$d = donation raised; %2$s = currency
 	'DONATE_RECEIVED'             => 'Nous avons reçu <strong>%1$d</strong> %2$s de dons.',
@@ -50,19 +68,6 @@ $lang = array_merge($lang, array(
 	'DONATE_USED'                 => 'Les dons ont été utilisé à hauteur de <strong>%1$d</strong> %2$s des <strong>%3$d</strong> %2$s déjà reçus.',
 	'DONATE_USED_EXCEEDED'        => 'Nous avons utilisé <strong>%1$d</strong> %2$s. Tous les dons ont été utilisés.',
 	'DONATE_NOT_USED'             => 'Les dons n’ont pas été utilisés.',
-
-	// Pages
-	'PPDE_DONATION_TITLE'         => 'Faire un don',
-	'PPDE_DONATION_TITLE_HEAD'    => 'Faire un don pour',
-	'PPDE_CANCEL_TITLE'           => 'Dons annulés',
-	'PPDE_SUCCESS_TITLE'          => 'Dons validés',
-	'PPDE_CONTACT_PAYPAL'         => 'Connexion à PayPal - Veuillez patienter…',
-
-	// Header page
-	'PPDE_HEADER_LINK_TITLE'      => 'Faire un don',
-
-	// Index page
-	'PPDE_INDEX_STATISTICS_TITLE' => 'Statistiques des dons',
 
 	// Viewonline
 	'PPDE_VIEWONLINE'             => 'Consulte la page des dons',
