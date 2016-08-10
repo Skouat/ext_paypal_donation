c9518cd6265f1c183a76e4b9d5de247fa18bdc37
Skouat <forum@skouat.org>
2016-08-10 18:00:49 +0200

    Add text reparser for donation pages

---
+++ migrations/v32x/v320_m1_reparse.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ *
+ * PayPal Donation extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2015 Skouat
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+namespace skouat\ppde\migrations\v32x;
+
+class v320_m1_reparse extends \phpbb\db\migration\container_aware_migration
+{
+	/**
+	 * @inheritDoc
+	 */
+	static public function depends_on()
+	{
+		return array(
+			'\skouat\ppde\migrations\v31x\v310_m3_currency_data',
+		);
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function update_data()
+	{
+		return array(
+			array('custom', array(array($this, 'reparse'))),
+		);
+	}
+
+	/**
+	 * Run the ppde donation pages text reparser
+	 *
+	 * @param int $current A donation page identifier
+	 *
+	 * @return bool|int A donation page identifier or true if finished
+	 */
+	public function reparse($current = 0)
+	{
+		$reparser = new \skouat\ppde\textreparser\plugins\donation_pages_text(
+			$this->db,
+			$this->container->getParameter('core.table_prefix') . 'ppde_donation_pages'
+		);
+
+		if (empty($current))
+		{
+			$current = $reparser->get_max_id();
+		}
+
+		$limit = 50; // lets keep the reparsing conservative
+		$start = max(1, $current + 1 - $limit);
+		$end   = max(1, $current);
+
+		$reparser->reparse_range($start, $end);
+
+		$current = $start - 1;
+
+		return ($current === 0) ? true : $current;
+	}
+}
---
+++ textreparser/plugins/donation_pages_text.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ *
+ * PayPal Donation extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2015 Skouat
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+namespace skouat\ppde\textreparser\plugins;
+
+class donation_pages_text extends \phpbb\textreparser\row_based_plugin
+{
+	/**
+	 * {@inheritdoc}
+	 */
+	public function get_columns()
+	{
+		return array(
+			'id'			=> 'page_id',
+			'text'			=> 'page_content',
+			'bbcode_uid'	=> 'page_content_bbcode_uid',
+			'options'		=> 'page_content_bbcode_options',
+		);
+	}
+}
