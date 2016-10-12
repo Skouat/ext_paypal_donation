288fd95ef3d755c92841e15db60b0ea64a9bc705
kasimi <mail@kasimi.net>
2016-10-12 22:23:52 +0200

    Converted remaining tabs to spaces

--- controller/ipn_listener.php
+++ controller/ipn_listener.php
@@ -934,7 +934,7 @@ class ipn_listener
 			 * Event that is triggered when a transaction has been successfully completed
 			 *
 			 * @event skouat.ppde.do_actions_completed_before
-			 * @var	array   transaction_data    Array containing transaction data
+			 * @var array   transaction_data    Array containing transaction data
 			 * @since 1.0.3
 			 */
 			$vars = array(
@@ -977,8 +977,8 @@ class ipn_listener
 		 * Event to modify data before a user is added to the donors group
 		 *
 		 * @event skouat.ppde.donors_group_user_add_before
-		 * @var bool    can_use_autogroup	Whether or not to add the user to the group
-		 * @var	int     group_id            The ID of the group to which the user will be added
+		 * @var bool    can_use_autogroup   Whether or not to add the user to the group
+		 * @var int     group_id            The ID of the group to which the user will be added
 		 * @var int     payer_id            The ID of the user who will we added to the group
 		 * @var string  payer_username      The user name
 		 * @var bool    default_group       Whether or not the group should be made default for the user
