670eeb8748cac35b0775b42ae9bae21e06348873
Skouat <forum@skouat.org>
2018-12-21 17:55:54 +0100

    Fix check_donors_status() returned value

--- actions/core.php
+++ actions/core.php
@@ -264,7 +264,7 @@ class core
 	{
 		$this->payer_data = $this->ppde_operator_transaction->query_donor_user_data($type, $args);
 
-		return count($this->payer_data) == 0;
+		return (bool) count($this->payer_data);
 	}
 
 	/**
