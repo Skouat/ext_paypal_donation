bf79d189cab82500d75bd2e521535e5cace5d477
Skouat <forum@skouat.org>
2020-03-17 01:04:48 +0100

    Add a new predefined vars

--- actions/vars.php
+++ actions/vars.php
@@ -60,29 +60,23 @@ class vars
 	{
 		$default_currency_data = $this->actions_currency->get_default_currency_data($this->config['ppde_default_currency']);
 		$this->dp_vars = [
-			0 => ['var'   => '{USER_ID}',
-				  'value' => $this->user->data['user_id'],
-			],
-			1 => ['var'   => '{USERNAME}',
-				  'value' => $this->user->data['username'],
-			],
-			2 => ['var'   => '{SITE_NAME}',
-				  'value' => $this->config['sitename'],
-			],
-			3 => ['var'   => '{SITE_DESC}',
-				  'value' => $this->config['site_desc'],
-			],
-			4 => ['var'   => '{BOARD_CONTACT}',
-				  'value' => $this->config['board_contact'],
-			],
-			5 => ['var'   => '{BOARD_EMAIL}',
-				  'value' => $this->config['board_email'],
-			],
-			6 => ['var'   => '{BOARD_SIG}',
-				  'value' => $this->config['board_email_sig'],
-			],
-			7 => ['var'   => '{GOAL}',
-				  'value' => $this->actions_currency->format_currency($this->config['ppde_goal'], $default_currency_data[0]['currency_iso_code'], $default_currency_data[0]['currency_symbol'], (bool) $default_currency_data[0]['currency_on_left'])],
+			0 => ['var' => '{USER_ID}', 'value' => $this->user->data['user_id']],
+			1 => ['var' => '{USERNAME}', 'value' => $this->user->data['username']],
+			2 => ['var' => '{SITE_NAME}', 'value' => $this->config['sitename']],
+			3 => ['var' => '{SITE_DESC}', 'value' => $this->config['site_desc']],
+			4 => ['var' => '{BOARD_CONTACT}', 'value' => $this->config['board_contact']],
+			5 => ['var' => '{BOARD_EMAIL}', 'value' => $this->config['board_email']],
+			6 => ['var' => '{BOARD_SIG}', 'value' => $this->config['board_email_sig']],
+			7 => ['var' => '{DONATION_GOAL}', 'value' => $this->actions_currency->format_currency(
+				$this->config['ppde_goal'],
+				$default_currency_data[0]['currency_iso_code'],
+				$default_currency_data[0]['currency_symbol'],
+				(bool) $default_currency_data[0]['currency_on_left'])],
+			8 => ['var'=> '{DONATION_RAISED}', 'value' => $this->actions_currency->format_currency(
+				$this->config['ppde_raised'],
+				$default_currency_data[0]['currency_iso_code'],
+				$default_currency_data[0]['currency_symbol'],
+				(bool) $default_currency_data[0]['currency_on_left'])]
 		];
 
 		if ($this->actions_core->is_in_admin())
