54c221a102af1137689288f9108a897173eefc09
Mario Skouat <Skouat@users.noreply.github.com>
2020-05-16 23:55:48 +0200

    New translations acp_donation_pages.php (French)

--- language/fr/acp_donation_pages.php
+++ language/fr/acp_donation_pages.php
@@ -58,7 +58,7 @@ $lang = array_merge($lang, array(
 	'PPDE_DP_PAGE'             => 'Type de page',
 
 	// Donation Page Template vars
-	'PPDE_DP_BOARD_CONTACT'    => 'E-mail de contact',
+	'PPDE_DP_BOARD_CONTACT'    => 'Adresse courriel de contact',
 	'PPDE_DP_BOARD_EMAIL'      => 'Adresse courriel du forum',
 	'PPDE_DP_BOARD_SIG'        => 'Signature du forum',
 	'PPDE_DP_PREDEFINED_VARS'  => 'Variables prédéfinies',
