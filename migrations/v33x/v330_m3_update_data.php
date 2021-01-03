16d168c4f990ac0535a4735b13fde87080b69882
Mario Skouat <forum@skouat.org>
2021-01-03 13:39:43 +0100

    Donors module: Add migration file

---
+++ migrations/v33x/v330_m3_update_data.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ *
+ * PayPal Donation extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2015-2020 Skouat
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+namespace skouat\ppde\migrations\v33x;
+
+class v330_m3_update_data extends \phpbb\db\migration\migration
+{
+	public static function depends_on()
+	{
+		return ['\skouat\ppde\migrations\v33x\v330_m2_update_data'];
+	}
+
+	public function update_data()
+	{
+		return [
+			['module.add', [
+				'acp',
+				'PPDE_ACP_DONATION',
+				[
+					'module_basename' => '\skouat\ppde\acp\ppde_module',
+					'modes'           => ['donors'],
+				],
+			]],
+		];
+	}
+}
