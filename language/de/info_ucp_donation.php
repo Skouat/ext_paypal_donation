57daf69835c7b8e97736485d8b1cb7e9ef2d138d
Mario Skouat <Skouat@users.noreply.github.com>
2022-07-24 18:50:35 +0200

    New translations info_ucp_donation.php (German)

---
+++ language/de/info_ucp_donation.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ *
+ * PayPal Donation extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2015-2020 Skouat
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = [];
+}
+
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+//
+// Some characters you may want to copy&paste:
+// ’ « » “ ” …
+//
+
+/**
+ * UCP notification system
+ */
+$lang = array_merge($lang, [
+	'NOTIFICATION_TYPE_PPDE_ADMIN_DONATION_ERRORS'   => 'Someone makes a donation with errors',
+	'NOTIFICATION_TYPE_PPDE_ADMIN_DONATION_RECEIVED' => 'Someone makes a donation',
+	'NOTIFICATION_TYPE_PPDE_DONATION_RECEIVED'       => 'A donation you made was received',
+]);
