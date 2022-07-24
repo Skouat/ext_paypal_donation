7664dc16ee19e49a44762d8dbbfc815f409c9550
Mario Skouat <Skouat@users.noreply.github.com>
2022-07-24 18:50:46 +0200

    New translations exceptions.php (Vietnamese)

---
+++ language/vi_VN/exceptions.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ *
+ * PayPal Donation extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2015-2020 Skouat
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = [];
+}
+
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+//
+// Some characters you may want to copy&paste:
+// ’ « » “ ” …
+//
+
+/**
+ * logs
+ */
+$lang = array_merge($lang, [
+	'EXCEPTION_INVALID_CONFIG_NAME' => 'The config name “%s” does not exist.',
+	'EXCEPTION_INVALID_FIELD'       => 'The field “%s” does not exist.',
+	'EXCEPTION_INVALID_USER_ID'     => 'The user ID “%d” does not exist.',
+	'EXCEPTION_OUT_OF_BOUNDS'       => 'The field “%1$s” received data beyond its bounds.',
+]);
