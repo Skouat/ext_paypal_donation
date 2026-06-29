4e2dbe7d07f71bf6b1cfa34a68a9a81d04f14cce
Mario Skouat <forum@skouat.org>
2026-06-30 01:54:10 +0200

    Tests: Add unit tests for ClientFactory, TemplateVars, and DonorList

---
+++ tests/api/client_factory_test.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ *
+ * PayPal Donation extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2015-2026 Skouat
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+namespace skouat\ppde\tests\api;
+
+class client_factory_test extends \phpbb_test_case
+{
+	/** @var \skouat\ppde\api\paypal\client_factory */
+	protected $factory;
+	/** @var \phpbb\config\config */
+	protected $config;
+
+	protected function setUp(): void
+	{
+		parent::setUp();
+
+		$this->config = new \phpbb\config\config([
+			'ppde_rest_client_id'         => 'LIVE_CLIENT_ID_123',
+			'ppde_rest_secret'            => 'LIVE_SECRET_456',
+			'ppde_sandbox_rest_client_id' => 'SANDBOX_CLIENT_ID_789',
+			'ppde_sandbox_rest_secret'    => 'SANDBOX_SECRET_abc',
+		]);
+
+		$language = $this->createMock(\phpbb\language\language::class);
+
+		$this->factory = new \skouat\ppde\api\paypal\client_factory($this->config, $language);
+	}
+
+	public function test_is_configured_with_valid_credentials()
+	{
+		// Live is configured
+		$this->assertTrue($this->factory->is_configured(false));
+
+		// Sandbox is configured
+		$this->assertTrue($this->factory->is_configured(true));
+	}
+
+	public function test_is_not_configured_when_missing()
+	{
+		$this->config->set('ppde_rest_client_id', '');
+		$this->config->set('ppde_rest_secret', '');
+		$this->assertFalse($this->factory->is_configured(false));
+
+		$this->config->set('ppde_sandbox_rest_client_id', 'SOMETHING');
+		$this->config->set('ppde_sandbox_rest_secret', '');
+		$this->assertFalse($this->factory->is_configured(true));
+	}
+
+	public function test_get_client_id()
+	{
+		// Returns live client ID
+		$this->assertSame('LIVE_CLIENT_ID_123', $this->factory->get_client_id(false));
+
+		// Returns sandbox client ID
+		$this->assertSame('SANDBOX_CLIENT_ID_789', $this->factory->get_client_id(true));
+	}
+
+	public function test_get_credentials_matching_env()
+	{
+		$method = new \ReflectionMethod($this->factory, 'get_credentials');
+		$method->setAccessible(true);
+
+		// Live credentials
+		$live = $method->invoke($this->factory, false);
+		$this->assertSame('LIVE_CLIENT_ID_123', $live[0]);
+		$this->assertSame('LIVE_SECRET_456', $live[1]);
+
+		// Sandbox credentials
+		$sandbox = $method->invoke($this->factory, true);
+		$this->assertSame('SANDBOX_CLIENT_ID_789', $sandbox[0]);
+		$this->assertSame('SANDBOX_SECRET_abc', $sandbox[1]);
+	}
+}
---
+++ tests/controller/main_donor_list_logic_test.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ *
+ * PayPal Donation extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2015-2026 Skouat
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+namespace skouat\ppde\tests\controller;
+
+class main_donor_list_logic_test extends \phpbb_test_case
+{
+	/** @var \skouat\ppde\controller\main_donor_list */
+	protected $controller;
+
+	protected function setUp(): void
+	{
+		parent::setUp();
+
+		$config = new \phpbb\config\config([]);
+		$container = $this->createMock(\Symfony\Component\DependencyInjection\ContainerInterface::class);
+		$helper = $this->createMock(\phpbb\controller\helper::class);
+		$language = $this->createMock(\phpbb\language\language::class);
+		$auth = $this->createMock(\skouat\ppde\actions\auth::class);
+		$currency = $this->createMock(\skouat\ppde\actions\currency::class);
+		$request = $this->createMock(\phpbb\request\request::class);
+		$template = $this->createMock(\phpbb\template\template::class);
+		$user = $this->createMock(\phpbb\user::class);
+		$user_loader = $this->createMock(\phpbb\user_loader::class);
+
+		$this->controller = new \skouat\ppde\controller\main_donor_list(
+			$config, $container, $helper, $language, $auth, $currency,
+			$request, $template, $user, $user_loader, './', 'php'
+		);
+	}
+
+	public function url_delim_data()
+	{
+		return [
+			'empty params'     => ['https://foo.com/list', [], 'https://foo.com/list?'],
+			'with parameters'  => ['https://foo.com/list', ['sk=u', 'sd=a'], 'https://foo.com/list&amp;'],
+		];
+	}
+
+	/** @dataProvider url_delim_data */
+	public function test_set_url_delim($url, $params, $expected)
+	{
+		$method = new \ReflectionMethod($this->controller, 'set_url_delim');
+		$method->setAccessible(true);
+
+		$this->assertSame($expected, $method->invoke($this->controller, $url, $params));
+	}
+
+	public function sort_key_data()
+	{
+		return [
+			'matching ascending'  => ['u', 'u', 'a', 'd'],
+			'matching descending' => ['u', 'u', 'd', 'a'],
+			'mismatch keys'       => ['u', 'a', 'a', 'a'],
+		];
+	}
+
+	/** @dataProvider sort_key_data */
+	public function test_set_sort_key($sk, $sk_comp, $sd, $expected)
+	{
+		$method = new \ReflectionMethod($this->controller, 'set_sort_key');
+		$method->setAccessible(true);
+
+		$this->assertSame($expected, $method->invoke($this->controller, $sk, $sk_comp, $sd));
+	}
+}
